dd1329262a6363226af124dc5d43f075b8ebd651
nhat_minh <minhhnn@stdio.asia>
2023-09-22 09:58:54 +0700

    Feature: Filter products by status

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -8,6 +8,7 @@ import {
   Pagination,
   Select,
   Stack,
+  TextField,
 } from "@mui/material";
 import ProductsTable from "./table";
 import { SearchOutlined } from "@mui/icons-material";
@@ -17,31 +18,47 @@ import { productActions } from "../../store/product/productSlice";
 import { totalPagePagination } from "../../utils/pagination";
 import { EPagination } from "../../types/enums/pagination";
 import { debounceSearch } from "../../utils/debounceSearch";
+import { EApprovalStatus } from "../../types/enums/product";
+import CustomButton from "../../components/share/CustomButton";
 
 const Products = () => {
   const dispatch = useAppDispatch();
   const [search, setSearch] = useState("");
+  const [productStatusLabel, setProductStatus] = useState("");
   const [type, setType] = useState("");
   const debounceSearchListProducts = useCallback(debounceSearch, []);
+  const [listStatus] = useState([
+    { label: "Đã xác nhận", value: EApprovalStatus.Approve },
+    { label: "Đã chặn", value: EApprovalStatus.Ban },
+    { label: "Chưa yêu cầu", value: EApprovalStatus.Pending },
+    { label: "Đã từ chối", value: EApprovalStatus.Reject },
+    { label: "Chờ duyệt", value: EApprovalStatus.Requesting },
+  ]);
+  const [params, setParams] = useState<{
+    limit: number;
+    page: number;
+    name?: string;
+    status?: string;
+  }>({ limit: EPagination.Limit, page: 1 });
+  const { pagination, loadingListProducts } = useAppSelector(
+    (state) => state.product
+  );
 
   const handleSearch = (value: string) => {
     setSearch(value);
     debounceSearchListProducts(value.trim(), setParams);
   };
-  const handleFilter = (value: string) => {
-    setType(value);
-  };
-
-  const [params, setParams] = useState({ limit: EPagination.Limit, page: 1 });
-  const { pagination, loadingListProducts } = useAppSelector(
-    (state) => state.product
-  );
 
   const handlePagination = (e: any, value: number) => {
     setParams((prevState) => {
       return { ...prevState, page: value };
     });
   };
+  const resetParams = useCallback(() => {
+    setParams({ limit: EPagination.Limit, page: 1 });
+    setSearch("");
+    setProductStatus("");
+  }, []);
 
   useEffect(() => {
     dispatch(productActions.getListProducts(params));
@@ -57,13 +74,7 @@ const Products = () => {
           alignItems: "center",
         }}
       >
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-start",
-          }}
-        >
+        <Stack direction="row" alignItems="center" spacing={2}>
           <FormControl sx={{ width: { xs: "100%", md: 400 } }}>
             <OutlinedInput
               color="secondary"
@@ -79,25 +90,35 @@ const Products = () => {
               onChange={(e) => handleSearch(e.target.value)}
             />
           </FormControl>
-          {/* <FormControl
-            sx={{ width: { xs: "100%", md: 150 }, marginLeft: "15px" }}
+          <TextField
+            sx={{ minWidth: "120px" }}
+            variant="outlined"
+            select
+            id="product-status"
+            label="Trạng thái"
+            value={productStatusLabel}
+            InputLabelProps={{ shrink: !!productStatusLabel }}
+            onChange={(e: any) => {
+              setParams((prevState) => ({
+                ...prevState,
+                status: e.target.value,
+              }));
+              setProductStatus(e.target.value);
+            }}
           >
-            <InputLabel color="secondary" id="demo-simple-select-label">
-              Loại
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={type}
-              onChange={(e) => handleFilter(e.target.value)}
-              label="Type"
-              defaultValue="ALL_STATUS"
-              color="secondary"
-            >
-              <MenuItem value="ALL_STATUS">Tất cả</MenuItem>
-            </Select>
-          </FormControl> */}
-        </Box>
+            {listStatus.map((status) => (
+              <MenuItem key={status.value} value={status.value}>
+                {status.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <CustomButton
+            color="error"
+            onClick={resetParams}
+            label="Xóa bộ lọc"
+            disabled={!search && !productStatusLabel}
+          />
+        </Stack>
       </Box>
       <ProductsTable />
       <Stack sx={{ py: "20px" }}>
