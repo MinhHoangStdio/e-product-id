522b4f3cfe22b9322eb865d5a14cd69c688334c5
vh2504 <dangvanhoa077@gmail.com>
2023-10-17 16:53:06 +0700

    update ui statistics

--- src/components/dashboard/StatisticsItemBase.tsx
+++ src/components/dashboard/StatisticsItemBase.tsx
@@ -16,9 +16,9 @@ export default function StatisticsItemBase({
     <Paper
       sx={{
         width: "100%",
-        minHeight: "150px",
-        borderRadius: "0px",
-        border: "1px solid #9ee8d5",
+        minHeight: "120px",
+        borderRadius: "5px",
+        border: "2px solid skyblue",
         bgcolor: "#f7fdfe",
         p: 2,
       }}
@@ -31,7 +31,7 @@ export default function StatisticsItemBase({
           {quantity}
         </Typography>
       </Stack>
-      <Box sx={{ overflow: "auto", height: "120px" }}>{BodyContent}</Box>
+      <Box sx={{ overflow: "auto", height: "100px" }}>{BodyContent}</Box>
     </Paper>
   );
 }
